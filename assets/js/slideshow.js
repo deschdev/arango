a112cbdba7a04c5e83c2f449caf4be2eb95f908e
deschdev <94028614+deschdev@users.noreply.github.com>
2024-06-17 21:10:44 -0400

    save state

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,23 +1,43 @@
 let slideIndex = 0;
-export const showSlides = () => {
+
+export const showSlides = (n) => {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  if (n !== undefined) {
+    slideIndex = n;
+  } else {
+    slideIndex++;
   }
 
-  slideIndex++;
   if (slideIndex > slides.length) {
-      slideIndex = 1
-  } 
+      slideIndex = 1;
+  } else if (slideIndex < 1) {
+      slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); 
 }
+
+// Set initial slide
+showSlides(slideIndex);
+
+// Add event listeners to dots
+document.querySelectorAll('.dot').forEach((dot, index) => {
+  dot.addEventListener('click', () => showSlides(index + 1));
+});
+
+// Automatically cycle through slides every 5 seconds
+setInterval(() => {
+  showSlides();
+}, 5000);
